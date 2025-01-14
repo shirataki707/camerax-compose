bcc82c911e3c6c926f1dae34ea7ec4d3fd570670
shogo.takasaki <stakasaki707@gmail.com>
2025-01-15 00:21:00 +0900

    Add request permission for camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "jp.shirataki707.camera"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "jp.shirataki707.camera"
--- app/src/main/java/jp/shirataki707/camera/MainActivity.kt
+++ app/src/main/java/jp/shirataki707/camera/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
 import jp.shirataki707.camera.ui.theme.CameraTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +32,46 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CameraTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CameraPreviewScreen()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraPreviewScreen(modifier: Modifier = Modifier) {
+    val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+    if (cameraPermissionState.status.isGranted) {
+        CameraPreviewContent()
+    } else {
+        Column(
+            modifier = modifier.fillMaxSize().wrapContentSize().widthIn(max = 480.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
+                // If the user has denied the permission but the rationale can be shown,
+                // then gently explain why the app requires this permission
+                "Whoops! Looks like we need your camera to work our magic!" +
+                    "Don't worry, we just wanna see your pretty face (and maybe some cats).  " +
+                    "Grant us permission and let's get this party started!"
+            } else {
+                // If it's the first time the user lands on this feature, or the user
+                // doesn't want to be asked again for this permission, explain that the
+                // permission is required
+                "Hi there! We need your camera to work our magic! ✨\n" +
+                    "Grant us permission and let's get this party started! \uD83C\uDF89"
+            }
+            Text(textToShow, textAlign = TextAlign.Center)
+            Spacer(Modifier.height(16.dp))
+            Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
+                Text("Unleash the Camera!")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun CameraPreviewContent() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CameraTheme {
-        Greeting("Android")
-    }
 }
